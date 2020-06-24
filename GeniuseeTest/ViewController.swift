d0e961f55fd47538905357d2daaceaf5e51120df
Alina <alibogzl@gmail.com>
2020-06-24 16:28:53 +0300

    Initial Commit

---
+++ GeniuseeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeniuseeTest
+//
+//  Created by Alina Bohuslavska on 24.06.2020.
+//  Copyright © 2020 Alina Bohuslavska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
